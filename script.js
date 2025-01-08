977d66febdde1fe012e235ba6b4bedb6aaa74dd4
mrHarshchoudhary <Harshchoudharyv@gmail.com>
2025-01-08 10:12:11 +0530

    remove node module

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 import ollama from "ollama";
+
 const response = await ollama.chat({
   model: "qwen2:0.5b",
   messages: [{ role: "user", content: "Generate marketing emails" }],
 });
 console.log(response.message.content);
+
+
+
+
